cd207757d2b7f4feff26602a041d650fb5c10ef9
KobayashiYoh <y.kobayashi@4nonome.com>
2022-11-23 21:09:38 +0900

    TextFieldとIconButtonを追加

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -10,6 +10,81 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 4.0,
+                ),
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: TextField(),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.headphones_outlined,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mic_none,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const RotatedBox(
+                quarterTurns: 1,
+                child: SizedBox(
+                  height: 32.0,
+                  child: Icon(
+                    Icons.compare_arrows_outlined,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 4.0,
+                ),
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: TextField(),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.headphones_outlined,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mic_none,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
